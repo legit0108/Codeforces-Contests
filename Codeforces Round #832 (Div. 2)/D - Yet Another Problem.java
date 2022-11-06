42cf12068061a0f97c73e601756804019637b143
legit0108 <legitgamer2345@gmail.com>
2022-11-06 20:02:19 +0530

    Codeforces Round #832 (Div. 2)

---
+++ Codeforces Round #832 (Div. 2)/D - Yet Another Problem.java
@@ -0,0 +1,448 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+  static StringBuilder sb;
+  static dsu dsu;
+  static long fact[];
+  static long mod = (long) (1e9 + 7);
+  static BufferedReader br;
+  static HashMap<Long, Long> map;
+  
+  /*
+  
+   . a[l:r]
+   . r-l+1 will be even or odd
+   . a[l:r] = 0 ~ a[l:r] equal
+   . if r-l+1 is odd we can chose entire array and perform operation
+   . applying operation on any L,R pair will give same result as applying operation on l,r 
+   . if r-l+1 is odd and xor of all elements is zero then ans is 1 else ans is -1 (edge case exists, if a[l:r] is all zeros then ans is 0)
+   . if r-l+1 is even, we cannot make all elements equal by performing operation on l,r so we break the even length array into two odd length subarrays
+   . the two odd length subarrays when solved independently can be made zero if and only if the xor of both the arrays is zero implying xor of initial array should also be zero
+   . we can say that for even length we require 2 operations (edge case exists, if a[l:r] is all zeros then ans is 0, if a[r] is 0 ans is 1, if a[l] is 0 ans is 1)
+   . regardless of the fact that r-l+1 is even or not, if xor of elements a[l:r] is not zero, ans is -1
+ 
+   Each query is answered in O(log(n)) time complexity
+   
+  */
+  
+  static void solve() throws Exception{
+    int n = i();
+    int q = i();
+    long a[] = new long[n+1];
+    long prefixXor[] = new long[n+1];
+    long prefixZeros[] = new long[n+1];
+    
+    for(int idx=1; idx<=n; idx++) a[idx] = l();
+    for(int idx=1; idx<=n; idx++){
+        prefixXor[idx] = prefixXor[idx-1]^a[idx];
+        prefixZeros[idx] = prefixZeros[idx-1]+((a[idx]==0)?1:0);
+    }
+    
+    HashMap<Long, ArrayList<Integer>> map[] = new HashMap[2];
+    map[0] = new HashMap();
+    map[1] = new HashMap();
+    
+    for(int idx=1; idx<=n; idx++){
+        int rem = idx%2;
+        if(map[rem].get(prefixXor[idx])==null) map[rem].put(prefixXor[idx], new ArrayList());
+        map[rem].get(prefixXor[idx]).add(idx);
+    }
+    
+    while(q-->0){
+        int l =i();
+        int r = i();
+        int len = r-l+1;
+        long prefXor = prefixXor[r] ^ prefixXor[l-1];
+        
+        if(prefXor!=0){
+            sb.append("-1\n");
+            continue;
+        }
+        
+        if(prefixZeros[r]-prefixZeros[l-1]==len){
+            sb.append("0\n");
+            continue;
+            
+        }
+        
+        if(len%2==1){
+            sb.append("1\n");
+            continue;
+        }
+        
+        if(a[r]==0 || a[l]==0){
+            sb.append("1\n");
+            continue;
+        }
+        
+        int rem = l%2;
+        ArrayList<Integer> list = map[rem].get(prefixXor[l-1]);
+        
+        if(list==null){
+            sb.append("-1\n");
+            continue;
+        }
+        
+        int idx = binarySearch(list, l);
+        
+        if(idx!=-1 && idx<r){
+            sb.append("2\n");
+        }else sb.append("-1\n");
+    }
+  }
+  
+  private static int binarySearch(List<Integer> list, int idx){
+      int ansIdx = -1;
+      int low = 0;
+      int high = list.size()-1;
+      
+      while(low<=high){
+          int mid = low + (high-low)/2;
+          
+          if(((int)list.get(mid))>idx){
+              ansIdx = list.get(mid);
+              high = mid-1;
+          }else low = mid+1;
+      }
+      
+      return ansIdx;
+  }
+  
+  public static void main(String[] args) throws Exception{
+    sb = new StringBuilder();
+    br = new BufferedReader(new InputStreamReader(System.in));
+    int t = 1;
+    
+    while (t-- > 0) {
+      solve();
+    }
+    
+    out.printLine(sb);
+    out.close();
+  }
+
+  /*
+   * fact=new long[(int)1e6+10]; fact[0]=fact[1]=1; for(int i=2;i<fact.length;i++)
+   * { fact[i]=((long)(i%mod)1L(long)(fact[i-1]%mod))%mod; }
+   */
+//**************NCR%P******************
+  static long ncr(int n, int r) {
+    if (r > n)
+      return (long) 0;
+
+    long res = fact[n] % mod;
+    res = ((long) (res % mod) * (long) (p(fact[r], mod - 2) % mod)) % mod;
+    res = ((long) (res % mod) * (long) (p(fact[n - r], mod - 2) % mod)) % mod;
+    return res;
+
+  }
+
+  static long p(long x, long y)// POWER FXN MODULO //
+  {
+    if (y == 0)
+      return 1;
+
+    long res = 1;
+    while (y > 0) {
+      if (y % 2 == 1) {
+        res = (res * x) % mod;
+        y--;
+      }
+
+      x = (x * x) % mod;
+      y = y / 2;
+
+    }
+    return res;
+  }
+
+  // *************Disjoint set
+  // union*********//
+  static class dsu {
+    int parent[];
+
+    dsu(int n) {
+      parent = new int[n];
+      for (int i = 0; i < n; i++)
+        parent[i] = -1;
+    }
+
+    int find(int a) {
+      if (parent[a] < 0)
+        return a;
+      else {
+        int x = find(parent[a]);
+        parent[a] = x;
+        return x;
+      }
+    }
+
+    void merge(int a, int b) {
+      a = find(a);
+      b = find(b);
+      if (a == b)
+        return;
+      parent[b] = a;
+    }
+  }
+
+//**************PRIME FACTORIZE **********************************//
+  static TreeMap<Integer, Integer> prime(long n) {
+    TreeMap<Integer, Integer> h = new TreeMap<>();
+    long num = n;
+    for (int i = 2; i <= Math.sqrt(num); i++) {
+      if (n % i == 0) {
+        int nt = 0;
+        while (n % i == 0) {
+          n = n / i;
+          nt++;
+        }
+        h.put(i, nt);
+      }
+    }
+    if (n != 1)
+      h.put((int) n, 1);
+    return h;
+
+  }
+
+//****CLASS PAIR ************************************************
+  static class Pair implements Comparable<Pair> {
+    int x;
+    long y;
+
+    Pair(int x, long y) {
+      this.x = x;
+      this.y = y;
+    }
+
+    public int compareTo(Pair o) {
+      return (int) (this.y - o.y);
+
+    }
+
+  }
+
+  static class InputReader {
+    private InputStream stream;
+    private byte[] buf = new byte[1024];
+    private int curChar;
+    private int numChars;
+    private SpaceCharFilter filter;
+
+    public InputReader(InputStream stream) {
+      this.stream = stream;
+    }
+
+    public int read() {
+      if (numChars == -1)
+        throw new InputMismatchException();
+      if (curChar >= numChars) {
+        curChar = 0;
+        try {
+          numChars = stream.read(buf);
+        } catch (IOException e) {
+          throw new InputMismatchException();
+        }
+        if (numChars <= 0)
+          return -1;
+      }
+      return buf[curChar++];
+    }
+
+    public int Int() {
+      int c = read();
+      while (isSpaceChar(c))
+        c = read();
+      int sgn = 1;
+      if (c == '-') {
+        sgn = -1;
+        c = read();
+      }
+      int res = 0;
+      do {
+        if (c < '0' || c > '9')
+          throw new InputMismatchException();
+        res *= 10;
+        res += c - '0';
+        c = read();
+      } while (!isSpaceChar(c));
+      return res * sgn;
+    }
+
+    public String String() {
+      int c = read();
+      while (isSpaceChar(c))
+        c = read();
+      StringBuilder res = new StringBuilder();
+      do {
+        res.appendCodePoint(c);
+        c = read();
+      } while (!isSpaceChar(c));
+      return res.toString();
+    }
+
+    public boolean isSpaceChar(int c) {
+      if (filter != null)
+        return filter.isSpaceChar(c);
+      return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+    }
+
+    public String next() {
+      return String();
+    }
+
+    public interface SpaceCharFilter {
+      public boolean isSpaceChar(int ch);
+    }
+  }
+
+  static class OutputWriter {
+    private final PrintWriter writer;
+
+    public OutputWriter(OutputStream outputStream) {
+      writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(outputStream)));
+    }
+
+    public OutputWriter(Writer writer) {
+      this.writer = new PrintWriter(writer);
+    }
+
+    public void print(Object... objects) {
+      for (int i = 0; i < objects.length; i++) {
+        if (i != 0)
+          writer.print(' ');
+        writer.print(objects[i]);
+      }
+    }
+
+    public void printLine(Object... objects) {
+      print(objects);
+      writer.println();
+    }
+
+    public void close() {
+      writer.close();
+    }
+
+    public void flush() {
+      writer.flush();
+    }
+  }
+
+  static InputReader in = new InputReader(System.in);
+  static OutputWriter out = new OutputWriter(System.out);
+
+  public static long[] sort(long[] a2) {
+    int n = a2.length;
+    ArrayList<Long> l = new ArrayList<>();
+    for (long i : a2)
+      l.add(i);
+    Collections.sort(l);
+    for (int i = 0; i < l.size(); i++)
+      a2[i] = l.get(i);
+    return a2;
+  }
+
+  public static char[] sort(char[] a2) {
+    int n = a2.length;
+    ArrayList<Character> l = new ArrayList<>();
+    for (char i : a2)
+      l.add(i);
+    Collections.sort(l);
+    for (int i = 0; i < l.size(); i++)
+      a2[i] = l.get(i);
+    return a2;
+  }
+
+//*************NORMAL POWER******************************
+  public static long pow(long x, long y) {
+    long res = 1;
+    while (y > 0) {
+      if (y % 2 != 0) {
+        res = (res * x);// % modulus;
+        y--;
+
+      }
+      x = (x * x);// % modulus;
+      y = y / 2;
+    }
+    return res;
+  }
+
+//GCD___+++++++++++++++++++++++++++++++
+  public static long gcd(long x, long y) {
+    if (x == 0)
+      return y;
+    else
+      return gcd(y % x, x);
+  }
+
+
+
+
+
+
+
+  // ******LOWEST COMMON MULTIPLE
+  // *********************************************
+
+  public static long lcm(long x, long y) {
+    return (x * (y / gcd(x, y)));
+  }
+
+  // ****BINARY SEARCH****//
+
+  private static long bins(long arr[], long tar) {
+    int low = 0;
+    int high = arr.length - 1;
+
+    while (low <= high) {
+      int mid = low + (high - low) / 2;
+      if (arr[mid] > tar) high = mid - 1;
+      else if (arr[mid] < tar) low = mid + 1;
+      else return mid;;
+    }
+
+    return -1;
+  }
+
+  // ********COUNTER******//
+
+  private static void count(long arr[]) {
+    map = new HashMap();
+    for (long val : arr) map.put(val, map.getOrDefault(val, 0l) + 1l);
+  }
+
+//INPUT PATTERN********************************************************
+  public static int i() {
+    return in.Int();
+  }
+
+  public static long l() {
+    String s = in.String();
+    return Long.parseLong(s);
+  }
+
+  public static String s() {
+    return in.String();
+  }
+
+  public static int[] readArrayi(int n) {
+    int A[] = new int[n];
+    for (int i = 0; i < n; i++) {
+      A[i] = i();
+    }
+    return A;
+  }
+
+  public static long[] readArray(long n) {
+    long A[] = new long[(int) n];
+    for (int i = 0; i < n; i++) {
+      A[i] = l();
+    }
+    return A;
+  }
+
+}
